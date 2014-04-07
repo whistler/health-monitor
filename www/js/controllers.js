7627c52488653b79318159300273a8066e8ca2a8
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2014-04-06 20:23:00 -0700

    Add Ionic framework

---
+++ www/js/controllers.js
@@ -0,0 +1,19 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('SettingsCtrl', function($scope) {
+})
+
+.controller('LoginCtrl', function($scope) {
+    
+});
