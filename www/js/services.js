43c12200a7aff8303f784f80763b2fcd7895df0c
Harsimran Singh Maan <maan.harry@gmail.com>
2014-04-14 00:46:37 -0700

    Add auth for firebase and use new url

--- www/js/services.js
+++ www/js/services.js
@@ -20,6 +20,16 @@ app.factory('Friends', function() {
   }
 });
 app.factory("UserInfoService", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://taycwf2hnde.firebaseio-demo.com/userData");
+    var ref = new Firebase("https://fiery-fire-1643.firebaseio.com/users/1/userInfo");
+	var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+		if (!error)
+		{
+			
+		}
+	});
+	auth.login('password', {
+	  email: 'health-monitor@googlegroups.com',
+	  password: 'teamlifely'
+	});
     return $firebase(ref);
   }]);
