64df3226e3c7d1130a4dcda3a945a5b7eaa03066
Huang <judezhu@repun.(none)>
2014-04-22 13:15:17 -0700

    add new bar graph.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -26,35 +26,70 @@ app.controller('DashCtrl', function($scope, EngineService, NotificationService) 
 	    datasets : [
 	      {
 	        fillColor : "rgba(100, 100, 100, 0.05)",
-//	        fillColor :"#FFFFFF",
 	        strokeColor : "#F7464A",
 	        pointColor : "#F7464A",
 	        pointStrokeColor : "#fff",
-	        data : [65,59,90,81]
+	        data : [40,50,50,50],
+	        title:"activity"
 	      },
-	      {	        fillColor : "rgba(100, 100, 100, 0.05)",
-//	        fillColor :"#FFFFFF",
+	      {	       
+	    	fillColor : "rgba(100, 100, 100, 0.05)",
 	        strokeColor : "#7D4F6D",
 	        pointColor : "#7D4F6D",
 	        pointStrokeColor : "#fff",
-	        data : [28,48,40,19]
+	        data : [6,8,8,8],
+	        title:"sleep"
 	      },
 	      {
 		        fillColor :"rgba(100, 100, 100, 0.05)",
 		        strokeColor : "#9D9B7F",
 		        pointColor : "#9D9B7F",
 		        pointStrokeColor : "#fff",
-		        data : [89,80,86,90]
+		        data : [80,81,90,100],
+	      	title:"heart-rate"
 		  },
 	      {
 		        fillColor :"rgba(100, 100, 100, 0.05)",
 		        strokeColor : "#949FB1",
 		        pointColor : "#949FB1",
 		        pointStrokeColor : "#fff",
-		        data : [120,115,130,120]
+		        data : [100,100,90,80],
+			title:"blood-pressure"
+		  
 		  }
 	    ]
 	  };
+	  
+	  $scope.barData = {
+			    labels : ["January","February","March","April"],
+			    datasets : [
+			      {
+//			        fillColor : "rgba(220,220,220,0.5)",
+			        fillColor : "F7464A",
+			        strokeColor : "rgba(100, 100, 100, 0.05)",
+			        data : [40,50,50,50],
+			      },
+			      {
+//			        fillColor : "rgba(151,187,205,0.5)",
+			        fillColor : "#7D4F6D",
+			        strokeColor : "rgba(100, 100, 100, 0.05)",
+			        data : [6,8,8,8],
+			      },
+			      {
+//				        fillColor : "rgb(157, 155, 127,0.5)",
+				        fillColor : "#9D9B7F",
+				        strokeColor : "rgba(100, 100, 100, 0.05)",
+				        data : [80,81,90,100],
+				      },
+				      {
+//				        fillColor : "rgb(148, 159, 177,0.5)",
+				        fillColor : "#949FB1",
+				        strokeColor : "rgba(100, 100, 100, 0.05)",
+				        data : [100,100,90,80]
+				      }
+
+			    ]
+			  };
 	  
 	// legend(document.getElementById("lineLegend"),  $scope.lineData);
 });
