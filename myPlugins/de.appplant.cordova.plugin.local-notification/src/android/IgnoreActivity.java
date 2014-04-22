46075791ac6099b47d1a28b3bbf8d5e7611142e6
jeffrey chen <jeffreychen2013@gmail.com>
2014-04-21 20:32:32 -0700

    Updated the Ignore Interval Setting Page

--- myPlugins/de.appplant.cordova.plugin.local-notification/src/android/IgnoreActivity.java
+++ myPlugins/de.appplant.cordova.plugin.local-notification/src/android/IgnoreActivity.java
@@ -8,13 +8,15 @@ import android.content.res.Resources;
 import android.view.View;
 import android.widget.Toast;
 import android.app.NotificationManager;
+/*import com.firebase.client.*;
+import com.firebase.simplelogin.*;*/
 
 public class IgnoreActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		
+
 		// dismiss the notification first
 		Intent intent = this.getIntent();
         int noti_id = intent.getIntExtra(Receiver.NOTIFICATION_ID, 0);
@@ -28,7 +30,50 @@ public class IgnoreActivity extends Activity {
     }
 
 	public void setInterval(View view) {
-		Toast.makeText(this, "Button ID: " + view.getId(), Toast.LENGTH_SHORT).show();
+		//final Context context = this;
+		// login to the firebase
+		/*Firebase ref = new Firebase("https://fiery-fire-1643.firebaseio.com/users/1");
+		SimpleLogin authClient = new SimpleLogin(ref, this);
+		authClient.loginWithEmail("health-monitor@googlegroups.com", "teamlifely", new SimpleLoginAuthenticatedHandler() {
+			@Override
+			public void authenticated(
+					com.firebase.simplelogin.enums.Error error, User user) {
+				if(error != null) {
+				  // There was an error logging into this account
+				  Toast.makeText(context, "Error happened!", Toast.LENGTH_SHORT).show();
+				}
+				else {
+				  // We are now logged in
+				  Toast.makeText(context, "Login Successfully!", Toast.LENGTH_SHORT).show();
+				}
+				
+			}
+		});*/
+		String str = "Ignored for ";
+		Resources res = this.getResources();
+		int id_oneDay = res.getIdentifier("ignore_oneDay", "id", this.getPackageName());
+		int id_threeDays = res.getIdentifier("ignore_threeDays", "id", this.getPackageName());
+		int id_oneWeek = res.getIdentifier("ignore_oneWeek", "id", this.getPackageName());
+		int id_twoWeeks = res.getIdentifier("ignore_twoWeeks", "id", this.getPackageName());
+		int id_oneMonth = res.getIdentifier("ignore_oneMonth", "id", this.getPackageName());
+
+		int btn_id = view.getId();
+
+		if (btn_id == id_oneDay) {
+			str += "one day";
+		} else if (btn_id == id_threeDays) {
+			str += "three days";
+		} else if (btn_id == id_oneWeek) {
+			str += "one week";
+		} else if (btn_id == id_twoWeeks) {
+			str += "two weeks";
+		} else if (btn_id == id_oneMonth) {
+			str += "one month";
+		} else {
+			str = "Ignored forever";
+		}
+
+		Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
 		finish();
 	}
 
